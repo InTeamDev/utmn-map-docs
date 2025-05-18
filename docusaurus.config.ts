746876885a9bdc5434c6e10deace05801bac8619
ZetoOfficial <tpptgs@bk.ru>
2025-05-18 21:04:02 +0500

    add ci/cd

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,10 +10,10 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://docs.utmn-map.zetoqqq.ru',
+  url: 'https://utmn-map.zetoqqq.ru',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
